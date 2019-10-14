450aedb61d9a97c211ee7bf0b791064bc3686129
ArifShariar <47409421+ArifShariar@users.noreply.github.com>
2019-10-15 00:55:00 +0600

    Up to : Tutorial 8 : Box Rotation, Camera implementation

--- src/engineTest/MainGameLoop.java
+++ src/engineTest/MainGameLoop.java
@@ -1,7 +1,10 @@
 package engineTest;
 
+import entity.Camera;
+import entity.Entity;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import models.RawModel;
@@ -17,11 +20,12 @@ public class MainGameLoop {
         DisplayManager.createDisplay();
 
         Loader loader = new Loader();
-        Renderer renderer = new Renderer();
+
 
         StaticShader shader = new StaticShader();
+        Renderer renderer = new Renderer(shader);
 
-       float[] vertices = {
+     /*  float[] vertices = {
                -.5f, 0.5f, 0,
                -.5f, -.5f, 0,
                .5f, -.5f, 0,
@@ -38,17 +42,103 @@ public class MainGameLoop {
                0,1, //v1
                1,1, //v2
                1,0 //v3
-       };
+       };*/
+
+        float[] vertices = {
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
+
+                -0.5f,0.5f,0.5f,
+                -0.5f,-0.5f,0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
+
+                0.5f,0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                -0.5f,-0.5f,0.5f,
+                -0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,0.5f,
+                -0.5f,0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,-0.5f,0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f
+
+        };
+
+        float[] textureCoords = {
+
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0
+
+
+        };
+
+        int[] indices = {
+                0,1,3,
+                3,1,2,
+                4,5,7,
+                7,5,6,
+                8,9,11,
+                11,9,10,
+                12,13,15,
+                15,13,14,
+                16,17,19,
+                19,17,18,
+                20,21,23,
+                23,21,22
+
+        };
         RawModel model = loader.loadToVAO(vertices,textureCoords, indices);
 
-        ModelTexture texture = new ModelTexture(loader.loadTexture("mario"));
-        TexturedModel texturedModel = new TexturedModel(model, texture);
+        TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("box")));
+
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,-5), 0, 0, 0,1);
+
+        Camera camera = new Camera();
 
         while (!Display.isCloseRequested()){
             // main game loop
+            // entity.increasePosition(0, 0, -0.01f);
+            entity.increaseRotation(1,1,0);
+            camera.move();
             renderer.prepare();
             shader.start();
-            renderer.render(texturedModel);
+            shader.loadViewMatrix(camera);
+            renderer.render(entity, shader);
             shader.stop();
             DisplayManager.updateDisplay();
         }
--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,6 +1,10 @@
 package shaders;
 
+import entity.Camera;
 import org.lwjgl.util.vector.Matrix4f;
+import toolBox.Maths;
+
+import java.util.Calendar;
 
 public class StaticShader extends ShaderProgram{
 
@@ -8,6 +12,8 @@ public class StaticShader extends ShaderProgram{
     private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
 
     private int location_transformationMatrix;
+    private int location_projectionMatrix;
+    private int location_viewMatrix;
 
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -23,9 +29,20 @@ public class StaticShader extends ShaderProgram{
     @Override
     protected void getAllUniformLocations(){
        location_transformationMatrix =  super.getUniformLocation("transformationMatrix");
+       location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+       location_viewMatrix = super.getUniformLocation("viewMatrix");
     }
 
     public void loadTransformationMatrix(Matrix4f matrix){
         super.loadMatrix(location_transformationMatrix, matrix);
+    }
+
+    public void loadProjectionMatrix(Matrix4f projection){
+        super.loadMatrix(location_projectionMatrix, projection);
+    }
+
+    public void loadViewMatrix(Camera camera){
+        Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+        super.loadMatrix(location_viewMatrix, viewMatrix);
     }
 }
